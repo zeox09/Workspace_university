3d4698a97d8b23bf5c1f564eb29220b07764d313
zeox09 <119711714+zeox09@users.noreply.github.com>
2023-09-15 19:11:04 -0300

    subida de archivos de operaciones en python

---
+++ operacoes.py
@@ -0,0 +1,8 @@
+# para as operaçoes matematicas em python a gente usa as operaçoes matematicas normais
+num1 = 5
+num2 = 24
+sumar = num1 + num2
+resta = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+print(sumar, "\n", resta, "\n", multi, "\n", div)
