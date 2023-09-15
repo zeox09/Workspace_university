87146668fd3d4c1cb9f200ea770cacbc14335681
zeox09 <119711714+zeox09@users.noreply.github.com>
2023-09-15 13:46:10 -0300

    creacion del archivo variables.py y actualizacion

---
+++ variables.py
@@ -0,0 +1,5 @@
+# em python as variaveis sao declaradas de forma dinamica
+variavel1 = 12  # tipo de dado integer
+variavel2 = "hello"  # tipo de dado string
+variavel3 = 12, 56  # tipo de dado float
+variavel4 = True  # tipo de dado boolean
