14fa274db7b4792d3012827498292d69f2ca73c0
zeox09 <119711714+zeox09@users.noreply.github.com>
2023-09-16 13:03:13 -0300

    calculadora basica en python

--- control-flujo/ex-calculadora.py
+++ control-flujo/ex-calculadora.py
@@ -1,5 +1,30 @@
 # en este script realizo una calculadora basica de loop infinito
-# ejercicio de manejo de loops con for
-for j in range(3):
-    for k in range(2):
-        print(f"{j}, {k}")
+print("Bienvenido a la calculadora")
+print("para salir escribe \"salir\"")
+print("las operaciones a realizar son Suma,Resta, Multi, Div")
+resultado = ""
+while True:
+    if not resultado:
+        resultado = input("ingrese numero: ")
+        if resultado.lower() == "salir":
+            break
+        resultado = int(resultado)
+    operacion = input("ingrese la operacion ")
+    if operacion.lower() == "salir":
+        break
+    n2 = input("Por favor ingresa el numero 2 ")
+    if n2.lower() == "salir":
+        break
+    n2 = int(n2)
+    if operacion.lower() == "suma":
+        resultado += n2
+    elif operacion.lower() == "resta":
+        resultado -= n2
+    elif operacion.lower() == "multi":
+        resultado *= n2
+    elif operacion.lower() == "div":
+        resultado /= n2
+    else:
+        print("La operacion ingresada no es valida")
+
+    print(f"el resultado es: {resultado}")
