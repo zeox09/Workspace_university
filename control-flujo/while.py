fa97628e7e7fd2a5471865ac04b788a38ef120e3
zeox09 <119711714+zeox09@users.noreply.github.com>
2023-09-16 11:35:34 -0300

    añadiendo while en python

---
+++ control-flujo/while.py
@@ -0,0 +1,12 @@
+# numero = 1
+
+# while numero < 100:
+#     print(numero)
+#     numero *= 2
+comando = ""
+while comando.lower() != "salir":
+    comando = input("$ ")
+    print(comando)
+# comandos infinitos
+# para los loops infiitos debe usarse un break al final de cada iteracion con un if esto para
+# que no haya un consumo exagerado de memoria
