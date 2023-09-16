c31258d6e2b0d66a56d47d39458e65ce747c13cc
zeox09 <119711714+zeox09@users.noreply.github.com>
2023-09-16 11:32:03 -0300

    for en python

---
+++ control-flujo/for.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    i += 1
+    print("jorge", i)
+for numero in range(5):
+    print(numero, numero * "Hello world")
+
+# For Else
+
+buscar = 10
+for numero1 in range(5):
+    print(numero1)
+    if numero1 == buscar:
+        print("encontrado", buscar)
+        break
+else:
+    print("no se encontró el numero buscado")
+
+for char in "hello python":
+    print(char)
+# imprime todos los caracteres de "ultimate python"
